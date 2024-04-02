b291ddb419e14f01215c7b52c9a830477902a6d1
Hafiz <mdnur.hafiz@outlook.com>
2024-04-02 09:14:59 +0800

    Build dist

--- src/composables/useMedias.ts
+++ src/composables/useMedias.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const url = 'https://api.hafiz.dev/wp-json/wp/v2/media/'
-import type Media from '@/types/Media.ts'
+import type { Media } from '@/types/Media.ts'
 
 export const useMedias = () => {
     const api = {
